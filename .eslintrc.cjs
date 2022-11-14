7c96ddf4398dec19bb18053b7ce66cedb83a34ff
alvaro.neto <alvaro.neto@ilegra.com>
2022-11-14 09:55:35 -0300

    remove linter rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,7 +20,6 @@ module.exports = {
   rules: {
     "vue/script-indent": ["warn", 2, { baseIndent: 1 }],
     "vue/component-name-in-template-casing": ["error", "kebab-case"],
-    "vue/component-options-name-casing": ["error", "kebab-case"],
     "vue/custom-event-name-casing": ["error", "kebab-case"],
     "vue/component-api-style": ["error", ["options"]],
     "vue/no-required-prop-with-default": ["error", { autofix: false }],
