d33b149da4bd8f82119d74718a0e8790a4d078eb
alvaro.neto <alvaro.neto@ilegra.com>
2022-11-14 09:47:24 -0300

    modify some linter rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,9 +22,7 @@ module.exports = {
     "vue/component-name-in-template-casing": ["error", "kebab-case"],
     "vue/component-options-name-casing": ["error", "kebab-case"],
     "vue/custom-event-name-casing": ["error", "kebab-case"],
-    "vue/define-emits-declaration": ["error", "type-based"],
-    "vue/define-props-declaration": ["error", "type-based"],
-    "vue/component-api-style": ["error", ["script-setup"]],
+    "vue/component-api-style": ["error", ["options"]],
     "vue/no-required-prop-with-default": ["error", { autofix: false }],
   },
 };
