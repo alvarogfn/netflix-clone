c1a42abcc96d50881af0dbd846c2598c2eca5779
alvaro.neto <alvaro.neto@ilegra.com>
2022-11-30 10:54:13 -0300

    feat: add typescript support to project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,3 @@
-/* eslint-env node */
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
@@ -6,13 +5,8 @@ module.exports = {
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
+    "@vue/eslint-config-typescript",
     "@vue/eslint-config-prettier",
-  ],
-  overrides: [
-    {
-      files: ["cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}"],
-      extends: ["plugin:cypress/recommended"],
-    },
   ],
   parserOptions: {
     ecmaVersion: "latest",
@@ -20,8 +14,11 @@ module.exports = {
   rules: {
     "vue/script-indent": ["warn", 2, { baseIndent: 1 }],
     "vue/component-name-in-template-casing": ["error", "kebab-case"],
+    "vue/component-options-name-casing": ["error", "kebab-case"],
     "vue/custom-event-name-casing": ["error", "kebab-case"],
-    "vue/component-api-style": ["error", ["options"]],
+    "vue/define-emits-declaration": ["error", "type-based"],
+    "vue/define-props-declaration": ["error", "type-based"],
+    "vue/component-api-style": ["error", ["script-setup"]],
     "vue/no-required-prop-with-default": ["error", { autofix: false }],
   },
 };
